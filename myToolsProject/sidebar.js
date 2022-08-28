8e2a599683772a2a34a0f5fa2683b529d3ad0e8b
6MA-606 <85861104+6MA-606@users.noreply.github.com>
2022-08-29 02:02:59 +0700

    new Sidebar

--- myToolsProject/sidebar.js
+++ myToolsProject/sidebar.js
@@ -1,3 +1,20 @@
-$(function(){
-    $("#content").load("./tools/headertext.html"); 
-});
+function clickSidenav() {
+    sidenav = document.getElementById("mySidenav");
+    sidebtn = document.getElementById("sidebtn");
+    content = document.getElementById("content");
+
+    if (sidenav.state == "closed") {
+        sidenav.style.width = "250px";
+        sidebtn.style.marginLeft = "250px";
+        sidebtn.style.tranform = "rotate(0deg)";
+        content.style.marginLeft = "350px";
+        sidenav.state = "opened";
+    } else {
+        sidenav.style.width = "0px";
+        sidebtn.style.marginLeft = "0px";
+        sidebtn.style.tranform = "rotate(180deg)";
+        content.style.marginLeft = "100px";
+        sidenav.state = "closed";
+    }
+    
+}
